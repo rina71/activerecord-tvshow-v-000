1122178acb0de65a9fad214b3253f07a906e34fe
Yurina Shin <yurinas@gmail.com>
2018-07-26 14:57:47 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,11 +1,11 @@
 class CreateShows < ActiveRecord::Migration
   def change
-    create_table :shows do |t|
+    create_table :shows do |t| binding.pry
       t.string :name
       t.string :network
       t.string :day
       t.integer :rating
-      binding.pry
+
     end
   end
 end
