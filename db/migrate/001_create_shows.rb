c39bac62ff6d1becfe58906062a0beecca5c33f4
Yurina Shin <yurinas@gmail.com>
2018-07-26 14:52:29 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,7 @@ class CreateShows < ActiveRecord::Migration
     create_table :shows do |t|
       t.string :name
       t.string :network
-      t.datetime :day
+      t.string :day
       t.integer :rating
     end
   end
